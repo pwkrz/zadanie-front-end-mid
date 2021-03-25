2e361744b5e14f43de06892bd596e6a4d6ac43a0
pwkrz <pg.krzeminski@gmail.com>
2021-03-25 15:35:03 +0100

    ostylowanie ikony wishlist

---
+++ src/js/app.ts
@@ -0,0 +1,86 @@
+//------------------
+// UTILS
+// -----------------
+interface ElementData {
+
+}
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        var request = new XMLHttpRequest();
+        request.onload = () => {
+            resolve(request);
+        };
+        request.onerror = err => {
+            reject(err);
+        };
+        request.open("get", "/data/products.json", true);
+        request.send();
+    })
+}
+const createElement = (nodeName: string, attrMap: {[attrName: string]: string}): HTMLElement => {
+    let _element = document.createElement(nodeName);
+    Object.keys(attrMap).forEach(attrName => {
+        _element.setAttribute(attrName, attrMap[attrName]);
+    });
+    return _element;
+}
+const applySentenceCase = (string: string) => (string[0].toUpperCase() + string.slice(1));
+// -----------------
+const ProductListWrapper: HTMLDivElement = document.querySelector('#productListWrapper');
+const ResultCount: HTMLSpanElement = document.querySelector('#resultCount');
+
+const generateProductCard = (productInfo: any): HTMLDivElement => {
+    let _card = createElement('div', {
+        class: 'product-card card mb-4',
+    });
+    let _productName = applySentenceCase(productInfo.name);
+    _card.innerHTML = `
+        <div class="product-card__wishlist-button position-absolute">
+            <button class="product-card__wishlist-button--button btn bg-white rounded-circle">
+                <span class="product-card__wishlist-button--icon text-danger">${ productInfo.isFav ? '&#9829;' : '&#9825;'}</span>
+            </button>
+        </div>
+        <img src="${productInfo.picture}" class="card-img-top" alt="${_productName}">
+        <div class="card-body">
+            <h5 class="card-title text-secondary mb-0">${_productName}</h5>
+            <p class="mb-2">
+                <small class="card-text">${productInfo.size}
+                    <span class="rating text-danger">${'&#9733;'.repeat(productInfo.rating)}</span>
+                </small>
+            </p>
+            <div class="d-flex align-items-baseline">
+                <h3 class="float-left mr-2">${productInfo.price}</h3>
+                <small class="text-muted font-weight-light mr-2"><s>${productInfo.oldPrice ? productInfo.oldPrice : ''}</s></small>
+                <small class="text-danger font-weight-light">${productInfo.oldPrice ? ('You save ' + productInfo.savings) : ''}</small>
+            </div>
+        </div>
+    `
+    return <HTMLDivElement>_card;
+}
+
+const generateProductCardColumn = (card: HTMLDivElement): HTMLDivElement => {
+    let _cardColumn = createElement('div', {class: 'col-4'});
+    _cardColumn.appendChild(card);
+    return <HTMLDivElement>_cardColumn;
+}
+
+const populateProductList = (productList: any[]) => {
+    productList.forEach(el => {
+        let card = generateProductCard(el);
+        let cardColumn = generateProductCardColumn(card);
+        ProductListWrapper.appendChild(cardColumn);
+    })
+}
+
+const populateResultCount = (count: number) => {
+    ResultCount.innerText = count.toString();
+}
+
+getData()
+    .then((response: any) => {
+        let productsList = JSON.parse(response.responseText);
+        console.log(productsList);
+        populateProductList(productsList);
+        populateResultCount(productsList.length)
+    })
+    .catch(err => console.log('app', err))
